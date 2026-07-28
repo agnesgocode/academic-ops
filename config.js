e89cfaa3d408baad034ec2ca457e8a8098a3ed18
agnesgocode <agnesparamithagosali.ruangguru@gmail.com>
2026-07-29 00:09:16 +0800

    remove insert token

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.ACOPS_CONFIG = {
   mppWriteApiUrl: "https://script.google.com/macros/s/AKfycbxGRhIIGjNNvv1xYONZQHMyr8yY1JrZl2zY8tyNuloOK_6sna-hTgbYbe50--B0r00XqQ/exec",
-  mppApiToken: "PASTE_YOUR_ACOPS_API_TOKEN_HERE"
+  mppApiToken: "acops-mpp-2026-K7x9pQ4mN8vR2"
 };
